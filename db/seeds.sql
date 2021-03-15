d635c61d350b8fd0dc72ab6815e561e30efb721f
adeha047 <dehaven7777@gmail.com>
2021-03-14 20:02:52 -0400

    added new SQL files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
+INSERT burgers (burger_name, devoured) VALUES ('Grilled Chicken Burger', FALSE);
+INSERT burgers (burger_name, devoured) VALUES ('Black Bean Burger', TRUE);
