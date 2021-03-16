84879b4aa95d24911af431383d53bc6aded180c6
adeha047 <dehaven7777@gmail.com>
2021-03-16 11:10:41 -0400

    updated CSS

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -30,7 +30,7 @@ router.post("/api/burgers", function (req, res) {
 router.put("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
-  console.log("condition", condition);
+  // console.log(req.params.id);
   // console.log(req.body)
 
   burger.updateOne(
@@ -46,6 +46,7 @@ router.put("/api/burgers/:id", function (req, res) {
         res.status(200).end();
       }
     })
+    
 });
 
 router.delete("/api/burgers/:id", (req, res) => {
